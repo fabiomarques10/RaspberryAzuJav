b1376e8fc8b9eb14f02c05b7e8d98203c848c476
fabiomarques10 <fabiomarques54@hotmail.com>
2025-11-24 18:10:08 +0000

    Add files via upload

---
+++ pitinkerjava/src/main/java/pt/tpsi/ad/pi4j/applications/Main.java
@@ -0,0 +1,63 @@
+package pt.tpsi.ad.pi4j.applications;
+
+import com.pi4j.Pi4J;
+import com.pi4j.io.gpio.digital.DigitalInput;
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalState;
+import com.pi4j.io.gpio.digital.PullResistance;
+
+
+
+
+public class Main {
+
+    private static final int PIN_BUTTON = 24; // PIN 18 = BCM 24
+    private static final int PIN_LED = 22; // PIN 15 = BCM 22
+
+    private static int pressCount = 0;
+
+
+    public static void main(String[] args) throws Exception {
+
+        var pi4j = Pi4J.newAutoContext();
+
+        var ledConfig = DigitalOutput.newConfigBuilder(pi4j)
+                .id("led")
+                .name("LED Flasher")
+                .address(PIN_LED)
+                .shutdown(DigitalState.LOW)
+                .initial(DigitalState.LOW)
+                .provider("pigpio-digital-output");
+        var led = pi4j.create(ledConfig);
+
+        var buttonConfig = DigitalInput.newConfigBuilder(pi4j)
+                .id("button")
+                .name("Press button")
+                .address(PIN_BUTTON)
+                .pull(PullResistance.PULL_DOWN)
+                .debounce(3000L)
+                .provider("pigpio-digital-input");
+        var button = pi4j.create(buttonConfig);
+        button.addListener(e -> {
+            if (e.state() == DigitalState.LOW) {
+                pressCount++;
+
+            }
+        });
+
+
+        while (pressCount < 5) {
+            if (led.equals(DigitalState.HIGH)) {
+
+                led.low();
+            } else {
+                led.high();
+            }
+            Thread.sleep(500 / (pressCount + 1));
+        }
+
+        pi4j.shutdown();
+    }
+}
+
+
